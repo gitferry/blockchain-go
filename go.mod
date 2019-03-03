5bf119d87e11a8a86ea01076dbcfebecf3a7c16e
gitferry <greferry@gmail.com>
2019-03-02 17:19:50 -0800

    Add proof of work

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/gitferry/blockchain-go
 
-go 1.12
-
 require rsc.io/quote v1.5.2
--- main.go
+++ main.go
@@ -1,57 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/gitferry/blockchain-go/blockchain"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("This is the genesis block", []byte{})
-}
-
-func CreateBlockchain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := CreateBlockchain()
+	chain := blockchain.InitBlockchain()
 
 	chain.AddBlock("The first block")
 	chain.AddBlock("The second block")
 	chain.AddBlock("The third block")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous block hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in the block: %s\n", block.Data)
 		fmt.Printf("Block hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
