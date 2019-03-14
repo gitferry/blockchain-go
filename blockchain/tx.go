c087958d435d0f18b5301b753ba3f0a6325a1a1b
greferry <greferry@gmail.com>
2019-03-13 23:57:55 -0700

    Add new input and output

--- blockchain/tx.go
+++ blockchain/tx.go
@@ -1,20 +1,42 @@
 package blockchain
 
+import (
+	"bytes"
+
+	"github.com/gitferry/blockchain-go/wallet"
+)
+
 type TxOutput struct {
-	Value  int
-	PubKey string
+	Value      int
+	PubKeyHash []byte
 }
 
 type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
+	ID        []byte
+	Out       int
+	Signature []byte
+	PubKey    []byte
 }
 
-func (input *TxInput) CanUnlock(data string) bool {
-	return input.Sig == data
+func NewTXOutput(value int, address string) *TxOutput {
+	txOutput := &TxOutput{value, nil}
+	txOutput.Lock([]byte(address))
+
+	return txOutput
 }
 
-func (output *TxOutput) CanBeUnlocked(data string) bool {
-	return output.PubKey == data
+func (in *TxInput) UsesKey(pubKeyHash []byte) bool {
+	lockingHash := wallet.PublicKeyHash(in.PubKey)
+
+	return bytes.Compare(lockingHash, pubKeyHash) == 0
+}
+
+func (out *TxOutput) Lock(address string) {
+	pubKeyHash := wallet.Base58Decode(address)
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
+	out.PubKeyHash = pubKeyHash
+}
+
+func (out *TxOutput) isLockedWithKey(pubKeyHash []byte) bool {
+	return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
 }
